97378eca8c7b48179c8125520f3eefdd1625740b
chardon55 <charlesdong_2000@outlook.com>
2022-03-17 21:32:26 +0800

    Add Tohoku University method

---
+++ gpacalc/tohoku.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+from . import gpabase as b
+
+
+class TohokuGPA(b.GPACalcModule):
+    '''
+        GPA calculation method (Cumulative) of Tohoku University since April 1, 2016
+        (UNTESTED)
+
+        For details: https://www.tohoku.ac.jp/en/academics/cn_gpa.html
+    '''
+
+    def calculate(df: pd.DataFrame, score_col: str, grade_weight_col: str) -> np.float:
+        df1 = df.copy()
+
+        df1.drop(index=df1[df1[score_col] < 60].index, inplace=True)
+        df1[score_col] = df1[score_col] // 10 - 5
+        df1.replace(df1[score_col] > 4, 4, inplace=True)
+
+        return (df1[score_col] * df1[grade_weight_col]).sum() / df[grade_weight_col].sum()
